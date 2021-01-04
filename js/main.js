26d4da946eaf9d944e0140cf4eaf884323fe6ecf
= <=>
2021-01-04 10:54:46 +0700

    Add more

--- js/main.js
+++ js/main.js
@@ -12,3 +12,26 @@ imgScroll[2].onclick = function () {
 imgScroll[3].onclick = function () {
   productImg.src = imgScroll[3].src;
 };
+
+function fillProduct(type, element) {
+  let tabs = document.getElementsByClassName("tab-item");
+  for (let i = 0; i < tabs.length; i++) {
+    tabs[i].style.border = "none";
+  }
+  element.style.border = "3px solid #000";
+  document.getElementById(type).style.display = "block";
+  switch (type) {
+    case "all":
+      document.getElementById("men").style.display = "none";
+      document.getElementById("women").style.display = "none";
+      break;
+    case "men":
+      document.getElementById("all").style.display = "none";
+      document.getElementById("women").style.display = "none";
+      break;
+    case "women":
+      document.getElementById("all").style.display = "none";
+      document.getElementById("men").style.display = "none";
+      break;
+  }
+}
